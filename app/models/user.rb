8bbbc965d20a9588b28b9efe3bdf9ddac569c35f
Shinsuke KUROKI <shinsukekuroki@gmail.com>
2024-06-11 17:28:35 +0900

    User is two factor authenticatable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :registerable,
+         :recoverable, :rememberable, :validatable,
+         :two_factor_authenticatable, otp_secret_encryption_key: ENV['OTP_SECRET_ENCRYPTION_KEY']
 end
