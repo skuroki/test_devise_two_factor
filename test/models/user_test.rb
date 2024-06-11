196f3befb99d4f76f9a0b5b843bcc47cec78b38f
Shinsuke KUROKI <shinsukekuroki@gmail.com>
2024-06-11 17:52:02 +0900

    Test is user is two factor recoverable

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User is two factor recoverable" do
+    user = User.create!(email: "test@example.com", password: "password")
+    recovery_codes = user.generate_otp_backup_codes!
+    assert_equal 5, recovery_codes.length
+    recovery_codes.each do |code|
+      validity = user.invalidate_otp_backup_code!(code)
+      assert_equal validity, true
+    end
+  end
 end
